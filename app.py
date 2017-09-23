53628e650a651f8cf52394346dae613bff6f158f
Max Z <max.zlotskiy@gmail.com>
2017-09-23 12:21:03 -0400

    added two more routes

--- app.py
+++ app.py
@@ -13,6 +13,15 @@ def makeLink(text, active):
 def home():
     return "Go to: " + makeLink("/", False) + makeLink("cool", True) + makeLink("funny", True) + "<br>" + makeHuge(":-)")
 
+@my_app.route('/cool')
+def cool():
+    return "Go to: " + makeLink('/', True) + makeLink('cool', False) + makeLink('funny', True) + "<br>" + makeHuge("B-)")
+
+@my_app.route('/funny')
+def funny():
+    return "Go to: " + makeLink('/', True) + makeLink('cool', True) + makeLink('funny', False) + "<br>" + makeHuge("xD")
+
+
 if __name__ == '__main__':
     my_app.debug = True #auto-reload when changed
     my_app.run() #run the web app
