836bb4d8490b867f3c8b49e15b92c5dc9d184c37
Max Z <max.zlotskiy@gmail.com>
2017-09-23 12:16:04 -0400

    added links and a huge emoticon

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 my_app = Flask(__name__)
 
+def makeHuge(text):
+    return '<span style="font-size:78px;">'+text+'</span>'
+def makeLink(text, active):
+    if active == True:
+        return '<a href="'+text+'">'+text+'</a>' + ('&nbsp;'*10)
+    return '<u>'+text+'</u>' + ('&nbsp;' * 10)
+
 @my_app.route('/') #127.0.0.1:5000
 def home():
-    return "hello"
+    return "Go to: " + makeLink("/", False) + makeLink("cool", True) + makeLink("funny", True) + "<br>" + makeHuge(":-)")
 
 if __name__ == '__main__':
     my_app.debug = True #auto-reload when changed
