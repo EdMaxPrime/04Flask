b8f8c29069c32b279fdfcc26ea6ae72c8dce0004
Max Z <max.zlotskiy@gmail.com>
2017-09-23 11:59:50 -0400

    most basic flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+my_app = Flask(__name__)
+
+@my_app.roate('/') #127.0.0.1:5000
+def home():
+    return "hello"
+
+if __name__ == '__main__':
+    my_app.debug = True #auto-reload when changed
+    my_app.run() #run the web app
